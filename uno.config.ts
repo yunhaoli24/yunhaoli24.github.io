85c66952a4b56ce841350017ab7f09da6cac0d77
li.yunhao <li.yunhao@foxmail.com>
2025-07-01 04:41:22 +0000

    update

--- uno.config.ts
+++ uno.config.ts
@@ -1,28 +1,20 @@
-import extractorMdc from '@unocss/extractor-mdc'
 import {
   defineConfig,
   presetAttributify,
   presetIcons,
   presetTypography,
   presetWind4,
-  transformerDirectives,
-  transformerVariantGroup,
 } from 'unocss'
 
 export default defineConfig({
   presets: [
     presetAttributify(),
     presetIcons(),
-    presetTypography(),
     presetWind4({
-      reset: false,
+      preflights: {
+        reset: false,
+      },
     }),
-  ],
-  transformers: [
-    transformerDirectives(),
-    transformerVariantGroup(),
-  ],
-  extractors: [
-    extractorMdc(),
+    presetTypography(),
   ],
 })
