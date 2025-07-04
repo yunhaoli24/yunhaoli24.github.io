55f6ba37a8ccc7228846a3b59132fdb6db33fa15
li.yunhao <li.yunhao@foxmail.com>
2025-07-04 08:56:25 +0000

    update

--- app/components/academic/About.vue
+++ app/components/academic/About.vue
@@ -18,13 +18,7 @@
         <div>
           <div class="text-base indent-8">
             <p>
-              Hello, my name is Yunhao Li, and I am currently serving as a
-              research assistant at Hong Kong University of Science and Technology, where I am
-              fortunate to work under the esteemed guidance of
-              <a
-                class="text-[--el-color-primary]"
-                href="https://people.ucas.ac.cn/~0058478"
-              >Prof. Qiong Wang</a>. My academic
+              Hello, my name is Yunhao Li. My academic
               journey commenced at Guangzhou University, where I
               pursued and obtained my Master’s degree in Artificial
               Intelligence. During this enriching period, I was mentored by
